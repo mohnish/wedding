5245ad05e4b4af6f5abb160e00245990d8fa2734
Mohnish Thallavajhula <i@mohni.sh>
2015-11-24 00:46:13 -0800

    strike 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-$('.magazine-toggle').on('click', function(e) {
-  e.preventDefault();
-  $(this).siblings('img').toggle('slow');
+$(function() {
+  $('.magazine-cover').hide();
+
+  $('.magazine-toggle').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    $this.siblings('.magazine-cover').show('slow');
+    $this.hide();
+  });
 });
